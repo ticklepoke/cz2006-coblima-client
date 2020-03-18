7089e3e60d270792a5046afaca15db73fa6604a6
Jun Jia <junjia2210@gmail.com>
2020-03-18 21:34:25 +0800

    Added Register + Refactored CSS
    CSS Guidelines: please start each CSS class name with component name to avoid unnecessary inheritance

--- src/components/title/title.js
+++ src/components/title/title.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import './title.css';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./title.css";
 
 class Title extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( <h1 className="coblima-header" style={{color: this.props.color}} >COBLIMA</h1> );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Link to="/">
+        {" "}
+        <h1 className="coblima-title" style={{ color: this.props.color }}>
+          COBLIMA
+        </h1>{" "}
+      </Link>
+    );
+  }
 }
- 
+
 export default Title;
--- src/routes/register/register.js
+++ src/routes/register/register.js
@@ -1,15 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import "./register.css";
+import LoginLogo from "../../images/login.svg";
+import Title from "../../components/title/title";
+import { Link } from "react-router-dom";
+import Inputbar from "../../components/inputbar/inputbar";
+import Submitbutton from "../../components/submitbutton/submitbutton";
 
 class Register extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return ( <h1>
-            Register
-        </h1> );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="register-container">
+        <div className="register-left-column">
+          <Title />
+          <img src={LoginLogo} alt="Login-Logo" className="register-logo" />
+        </div>
+
+        <div className="register-right-column">
+          <div className="register-right-container">
+            <h2 className="register-header">Register</h2>
+            <Link to="/login">
+              <p className="register-subheader">Click here for login</p>
+            </Link>
+
+            <form>
+              <Inputbar text="Username" />
+              <Inputbar text="Matriculation Number" />
+              <Inputbar text="Password" />
+              <Inputbar text="Confirm Password" />
+              <Submitbutton text="Register" />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Register;
