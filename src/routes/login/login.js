13da2da83819c7e189df20d40c7461158dcb7be2
Nigel Lee <lee.nigel@hotmail.com>
2020-03-23 22:51:07 +0800

    changed login push route

--- src/routes/login/login.js
+++ src/routes/login/login.js
@@ -67,7 +67,7 @@ class Login extends Component {
       .login(this.state.email, this.state.password)
       .then(res => {
         console.log(res);
-        this.props.history.push("course");
+        this.props.history.push("/");
       })
       .catch(err => {
         console.log(err);
